8a65316021bb3334d95db7c97c7061817de946b1
Steeve <steeve.dombald.pro@outlook.com>
2021-10-11 09:08:49 +0200

    ajust p2

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const dicePlayer2 = 'assets/face_' + secondRandomNum + '.png';
 document.querySelectorAll('img')[0].setAttribute('src', dicePlayer1);
 document.querySelectorAll('img')[1].setAttribute('src', dicePlayer2);
 
-
+// conditions de victoire
 if (firstRandomNum > secondRandomNum) {
     document.querySelector('h1').innerText = ("Player 1 win !")
 } else if (secondRandomNum > firstRandomNum) {
-    document.querySelector('h1').innerText = ("Player 2 won !")
+    document.querySelector('h1').innerText = ("Player 2 win !")
 } else {
     document.querySelector('h1').innerText = ("It's a draw...")
 }
